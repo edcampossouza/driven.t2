b627d611f24aa08fe768221f4e9412e8ee6340d2
Eduardo Souza <edcampossouza@gmail.com>
2023-04-20 17:29:51 -0300

    feat GET /hotels: return 404 when no ticket

--- src/services/hotels-service/errors.ts
+++ src/services/hotels-service/errors.ts
@@ -6,3 +6,10 @@ export function enrollmentNotFoundError(): ApplicationError {
     message: 'User does not have an enrollment yet',
   };
 }
+
+export function ticketNotFound(): ApplicationError {
+  return {
+    name: 'NotFoundError',
+    message: 'User does not have a ticket yet',
+  };
+}
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,13 +1,17 @@
 import { Enrollment, Hotel, Ticket, TicketType } from '@prisma/client';
-import { enrollmentNotFoundError } from './errors';
+import { enrollmentNotFoundError, ticketNotFound } from './errors';
 import hotelRepository from '@/repositories/hotels-repository';
 import enrollmentRepository from '@/repositories/enrollment-repository';
+import ticketRepository from '@/repositories/ticket-repository';
 
 export async function getHotels(userId: number): Promise<Hotel[]> {
   const hotels: Hotel[] = await hotelRepository.getHotels();
   //user has an enrollment?
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
   if (!enrollment) throw enrollmentNotFoundError();
+  //enrollment has a ticket?
+  const ticket = await ticketRepository.getUserTicket(userId);
+  if (!ticket) throw ticketNotFound();
   return hotels;
 }
 
