c5d563e4e6f96713b7962864b50c68dcee3c806f
Eduardo Souza <edcampossouza@gmail.com>
2023-04-20 19:59:23 -0300

    feat GET hotels: reorder ifs to comply with evaluator

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -6,20 +6,20 @@ import ticketRepository from '@/repositories/ticket-repository';
 import { paymentError } from '@/errors';
 
 export async function getHotels(userId: number): Promise<Hotel[]> {
-  const hotels: Hotel[] = await hotelRepository.getHotels();
   //user has an enrollment?
   const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
   if (!enrollment) throw enrollmentNotFoundError();
   //enrollment has a ticket?
   const ticket = await ticketRepository.getUserTicket(userId);
   if (!ticket) throw ticketNotFound();
-  //are there any hotels?
-  if (hotels.length < 1) throw noHotelsFound();
-
   const status = ticket.status;
   if (ticket.TicketType.isRemote) throw paymentError('Ticket is for a remote event');
   if (!ticket.TicketType.includesHotel) throw paymentError('Ticket does not include accomodation');
   if (status !== 'PAID') throw paymentError('Ticket not paid');
+
+  //are there any hotels?
+  const hotels: Hotel[] = await hotelRepository.getHotels();
+  if (hotels.length < 1) throw noHotelsFound();
   return hotels;
 }
 
